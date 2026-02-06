f44513b06914d50678928e772138d207a6f03b84
lazar403 <ldbelgradebiz@gmail.com>
2026-02-06 13:58:38 +0100

    add Spring boot 4 project

---
+++ backend/src/main/java/rs/lazar403/veloxauto/VeloxautoApplication.java
@@ -0,0 +1,13 @@
+package rs.lazar403.veloxauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloxautoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloxautoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/rs/lazar403/veloxauto/VeloxautoApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.lazar403.veloxauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloxautoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
