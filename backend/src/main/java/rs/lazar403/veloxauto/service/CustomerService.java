84cb797720827d9dfc8d9da013702427a207a563
lazar403 <ldbelgradebiz@gmail.com>
2026-02-08 18:38:57 +0100

    refactor (customer): update service to use DTO mapping, update controller to use DTO layer

--- backend/src/main/java/rs/lazar403/veloxauto/service/CustomerService.java
+++ backend/src/main/java/rs/lazar403/veloxauto/service/CustomerService.java
@@ -1,11 +1,15 @@
 package rs.lazar403.veloxauto.service;
 
 
+import rs.lazar403.veloxauto.dto.customer.CustomerCreateRequest;
+import rs.lazar403.veloxauto.dto.customer.CustomerResponse;
 import rs.lazar403.veloxauto.model.Customer;
 
 import java.util.List;
 
 public interface CustomerService {
-    Customer addCustomer(Customer customer);
-    List<Customer> getAllCustomers();
+
+    CustomerResponse createCustomer(CustomerCreateRequest request);
+
+    List<CustomerResponse> getAllCustomers();
 }
--- backend/src/main/java/rs/lazar403/veloxauto/service/impl/CustomerServiceImpl.java
+++ backend/src/main/java/rs/lazar403/veloxauto/service/impl/CustomerServiceImpl.java
@@ -1,7 +1,13 @@
 package rs.lazar403.veloxauto.service.impl;
 
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import rs.lazar403.veloxauto.dto.customer.CustomerCreateRequest;
+import rs.lazar403.veloxauto.dto.customer.CustomerResponse;
+import rs.lazar403.veloxauto.enums.CustomerRole;
+import rs.lazar403.veloxauto.mapper.CustomerMapper;
 import rs.lazar403.veloxauto.model.Customer;
 import rs.lazar403.veloxauto.repository.CustomerRepository;
 import rs.lazar403.veloxauto.service.CustomerService;
@@ -11,17 +17,32 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class CustomerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
 
     @Override
-    public Customer addCustomer(Customer customer) {
-        return customerRepository.save(customer);
+    public CustomerResponse createCustomer(CustomerCreateRequest request) {
+        if (customerRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email already exists.");
+        }
+
+        Customer customer = customerMapper.toEntity(request);
+
+        // biz defaults
+        customer.setRole(CustomerRole.CUSTOMER);
+        customer.setActive(true);
+
+        Customer savedCustomer = customerRepository.save(customer);
+        return customerMapper.toResponse(savedCustomer);
     }
 
     @Override
-    public List<Customer> getAllCustomers() {
-        return customerRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<CustomerResponse> getAllCustomers() {
+        return customerMapper.toResponseList(customerRepository.findAll());
     }
+
 }
